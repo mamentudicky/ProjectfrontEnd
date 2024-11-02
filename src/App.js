bcf7ed6559e508eb96fe8cde015a87b4107eaf7b
mamentudicky <dickymamentu@gmail.com>
2024-11-03 00:32:46 +0800

    App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from "react";
-import Header from "./Header";
-import Home from "./Home";
-import Footer from "./Footer";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Home />
-      <Footer />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
