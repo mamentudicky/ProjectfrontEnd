74bed4ab103a2d8c5b20b81eb57294938acf05a2
mamentudicky <dickymamentu@gmail.com>
2024-11-04 23:45:39 +0800

    add CardInfo , Generatiions

---
+++ src/components/CardInfo/index.js
@@ -0,0 +1,15 @@
+import Evolution from "../Evolution";
+import TypeEffect from "../TypeEffect";
+import Logo from "../Logo";
+const CardInfo = () => {
+  return (
+    <div>
+      <h1>card-info</h1>
+      <Evolution />
+      <TypeEffect />
+      <Logo />
+    </div>
+  );
+};
+
+export default CardInfo;
