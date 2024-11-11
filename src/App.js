b94e6b6b4216696d208011e7d9bdefdfe589e409
mamentudicky <dickymamentu@gmail.com>
2024-11-11 22:57:49 +0800

    perbaiki folder eror

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import Header from './components/Header';
-import Home from './components/Home';
-import Footer from './components/Footer';
+import React from "react";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Footer from "./components/Footer"
 
 const App = () => {
   return (
     <div>
-      <Header />
-      <Home />
-      <Footer />
+      <Header title="Ini adalah Header" />
+      <Home title="Ini adalah home" />
+      <Footer title="Ini adalah Footer" />
     </div>
   );
 };
